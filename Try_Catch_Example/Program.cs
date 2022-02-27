a87d87603f53aa308911fcc96f6200fc9999a4b7
Erfan Mohajer Soltani <erfanm512@gmail.com>
2022-02-27 23:45:05 +0330

    controlling exception using try-catch (with comment description)

--- Try_Catch_Example/Program.cs
+++ Try_Catch_Example/Program.cs
@@ -6,7 +6,23 @@ namespace Try_Catch_Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int age;
+            // when everything is going ok the codes of try body will execute
+            try
+            {
+                Console.WriteLine("If you enter string app will throw exception, so just enter your age:");
+                age = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Congratulation you're " + age + " years old");
+            }
+            /* when app throw a exception the codes of catch body will execute and
+               also we can have multiple catches with different types of exception */
+
+            catch (FormatException)
+            {
+                Console.WriteLine("ERROR!!!");
+            }
+
         }
     }
 }
